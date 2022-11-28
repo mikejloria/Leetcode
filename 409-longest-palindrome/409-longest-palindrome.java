6a0c1cb9f59d983994263af70ae7af10139a14dc
MJ <mikejloria@gmail.com>
2022-11-27 23:19:19 -0800

    409-longest-palindrome

---
+++ 409-longest-palindrome/409-longest-palindrome.java
@@ -0,0 +1,8 @@
+class LongestPalindrome {
+    // for String problems think about Mappings as a solution
+    // palindrome is a String that can be read the same forward and backward
+    public int longPlaindrome (String s) {
+        HashSet hashSet = new HashSet();
+        return "";
+    }
+}
