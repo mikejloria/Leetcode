124b25300ce2f116d12ca8fd7c05019446dc3676
MJ <mikejloria@gmail.com>
2022-11-29 15:53:20 -0800

    589-N-ary-tree-preorder-traversal

--- 409-longest-palindrome/409-longest-palindrome.java
+++ 409-longest-palindrome/409-longest-palindrome.java
@@ -1,10 +1,11 @@
 import java.util.HashSet;
 
 class LongestPalindrome {
-    // for String problems think about Mappings as a solution
+    // for String problems think about Mapping structures as a possible solution
     // palindrome is a String that can be read the same forward and backward
+    // odd and even counts of characters
     public int longPlaindrome (String s) {
-        HashSet<Character> hashSet = new HashSet();
+        HashSet<Character> hashSet = new HashSet<Character>();
         int count = 0;
 
         for (int i = 0; i < s.length(); i++) {
---
+++ 589-N-ary-tree-preorder-traversal/589-N-ary-tree-preorder-traversal.java
@@ -0,0 +1,44 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+*/
+
+
+
+class NAryPreorderTraversal  {
+
+
+    // iterative	
+    public List<Integer> preorderIterative (Node root) {
+
+    }
+    
+
+
+    // recursive
+    public List<Integer> list = new ArrayList<>();	
+    public List<Integer> preorderRecursive (Node root) {
+
+	    if (root == null) return list;
+
+	    list.add(root.val);
+	    for (Node node : root.children)
+		    predorderRecursive(node);
+
+	    return list;
+    }
+}
